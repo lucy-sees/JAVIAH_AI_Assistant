652add31867bcf5edb1b7ae7e566b607e726320e
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2024-05-07 22:31:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,12 @@ import eel
 
 from engine.features import *
 from engine.command import *
-
-# def start():
     
-#     eel.init("www")
-
-#     playAssistantSound()
-
-
-#     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
-
-#     eel.start('index.html', mode=None, host='localhost', block=True)
-
-
-eel.init('www')  
+eel.init("www")
 
 # playAssistantSound()
 
-# @eel.expose
-# def hello():
-#     print('Hello from Python!')
 
-eel.start('index.html')  
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+eel.start('index.html', mode=None, host='localhost', block=True)
