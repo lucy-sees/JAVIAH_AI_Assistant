ec939bbbdc9a0d74eaddb124da80388838a02ec0
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2024-05-09 08:13:58 +0300

    update controller

--- www/controller.js
+++ www/controller.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
     eel.expose(DisplayMessage)
     function DisplayMessage(message) {
 
-        $(".siri-message li:first").text(message);
-        $('.siri-message').textillate('start');
+        $(".javiahs-message li:first").text(message);
+        $('.javiahs-message').textillate('start');
 
     }
 
